52b24d3c34fe56ac6c09dd4c4cebd5d56308177a
Madeleine Marcano <madeleine1302@gmail.com>
2018-09-20 18:46:10 -0300

    componentes Logo, SignIn, Login

---
+++ src/Components/Global/Logo.js
@@ -0,0 +1,15 @@
+//Dependencies
+import React from 'react';
+
+//Assets
+import logo from '../../img/logo.png';
+
+function Logo() {
+    return (
+        <div className='Logo'>
+            <img src={logo} alt={'Logo'} width='100%'/>
+        </div>
+    );
+}
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyASGPdlcAyM0wCqN4GtVaN5b_cygABwoeo",
+//   authDomain: "social-network-react-5f5c4.firebaseapp.com",
+//   databaseURL: "https://social-network-react-5f5c4.firebaseio.com",
+//   projectId: "social-network-react-5f5c4",
+//   storageBucket: "social-network-react-5f5c4.appspot.com",
+//   messagingSenderId: "31676279948"
+// })
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+

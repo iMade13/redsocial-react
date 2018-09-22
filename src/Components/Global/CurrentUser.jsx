0a364e98c722c7eedfc40fcee2e849bd49bd5d2e
Madeleine Marcano <madeleine1302@gmail.com>
2018-09-22 20:08:19 -0300

    componentes

---
+++ src/Components/Global/CurrentUser.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CurrentUser extends(Component) {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+        <h1></h1>  
+        )
+    }
+}
+    
+
+export default CurrentUser;
--- src/Components/Global/Header.jsx
+++ src/Components/Global/Header.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Logo from './Logo.js'
 
 class Header extends Component {
     render() {
         return (
             <div className="col-4 col-md-3 logo">
-                <Logo/>
+                <Logo />
             </div>
+            
 
         )
     }
